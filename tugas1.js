0ea28fd927597b9f27b15ab561e7c887ffd3f273
Irwan Dharmawan <lexiconrandalph@gmail.com>
2020-11-09 00:01:20 +0700

    trying code in tugas1.js

--- tugas1.js
+++ tugas1.js
@@ -1,5 +1,7 @@
-var first_name='irwan';
-let last_name='Dharmawan';
-const age=25
+// var first_name='irwan';
+// let last_name='Dharmawan';
+// const age=25
+//
+// console.log(first_name,last_name,'umur',age,'tahun')
 
-console.log(first_name,last_name,'umur',age,'tahun')
+console.log(Math.pow(5,0))
